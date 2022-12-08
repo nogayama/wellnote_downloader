8f6720b58e80027c3f8718a3c978a616e1ee1ec5
TAKAHIDE NOGAYAMA <NOGAYAMA@jp.ibm.com>
2022-12-09 03:11:45 +0900

    fix: issue that module wellnote_downloader is not found
    Signed-off-by: TAKAHIDE NOGAYAMA <NOGAYAMA@jp.ibm.com>

--- src/wellnote_downloader/__init__.py
+++ src/wellnote_downloader/__init__.py
@@ -10,7 +10,7 @@
 # http://opensource.org/licenses/mit-license.php
 # =================================================================
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 
 import argparse
 from argparse import ArgumentParser, Action, Namespace
